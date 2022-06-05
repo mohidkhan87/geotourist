2dbff7a0cf309b75e855feb49940eb8c74dcc318
mohidkhan87 <mohidkhan87@gmail.com>
2022-06-05 16:28:28 +0500

    project setup

---
+++ src/views/create/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Project Setup: Vue 2 + Vue Router + TailwindCSS
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
